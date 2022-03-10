938714ac665ed5741d0749f828e1bf299b2b7ddb
Maria <mfb171201@gmail.com>
2022-03-09 23:01:14 -0400

    a

--- src/loan/loan.service.ts
+++ src/loan/loan.service.ts
@@ -1,18 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateLoanDto } from './dto/requests/create-loan.dto';
 import { Loan } from './entities/loan.entity';
 
 @Injectable()
 export class LoanService {
-  logger: Logger;
   private loans: Loan[] = [];
   private id = 0;
 
-  constructor() {
-    this.logger = new Logger('LoanService');
-  }
   create(createLoanData: CreateLoanDto) {
-    this.logger.log(`Creating a new loan: ${JSON.stringify(createLoanData)}`);
     // Criar uma entrada no DB
     const loan: Loan = {
       id: this.id,
@@ -28,12 +23,6 @@ export class LoanService {
   findAll() {
     // TODO: ordenar os loans por categoria e por data de expiração
     // TODO: adicionar a lista ao fake DB
-    m;
-    a;
-    r;
-    i;
-    a;
-    r;
     return this.loans;
   }
 
@@ -43,22 +32,10 @@ export class LoanService {
 
   // TODO: implementar o update
   update(id: number, updateLoanDto: any) {
-    const loan = this.findOne(id);
-    this.loans = this.loans.map((v) => {
-      if (v.id === id) {
-        return {
-          ...loan,
-          ...updateLoanDto,
-        };
-      }
-      return v;
-    });
     return `This action updates a #${id} loan`;
   }
   // TODO : implementar o remove
   remove(id: number) {
-    const loan = this.findOne(id);
-    this.loans = this.loans.filter((v) => v.id !== id);
-    return loan;
+    return `This action removes a #${id} loan`;
   }
 }
